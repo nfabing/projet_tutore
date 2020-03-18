33aa73f00729ef89b9579dd52cfcf4d36693f048
Nicolas Fabing <nicofabing@gmail.com>
2020-03-18 17:02:15 +0100

    added firebase config.tsx

---
+++ src/config/config.tsx
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    // Your web app's Firebase configuration
+    apiKey: "AIzaSyATZ15AzLp3tW_wmVppJgNUfaDdCZ5_MmM",
+    authDomain: "projet-tutore-6833d.firebaseapp.com",
+    databaseURL: "https://projet-tutore-6833d.firebaseio.com",
+    projectId: "projet-tutore-6833d",
+    storageBucket: "projet-tutore-6833d.appspot.com",
+    messagingSenderId: "731792188004",
+    appId: "1:731792188004:web:30c70425548bbef545b571",
+    measurementId: "G-8NT6M5EKV4"
+};
+
+export default firebaseConfig
